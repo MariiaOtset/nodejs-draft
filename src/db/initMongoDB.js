329cb6a1723c7e05b16517f5f08da34cfab5142a
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-07-29 21:14:09 +0100

    errors fixed

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -10,7 +10,9 @@ export const initMongoDB = async () => {
     const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
-
+    console.log(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
     await mongoose.connect(
       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
