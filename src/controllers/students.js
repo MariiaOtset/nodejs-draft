34ad157887d69dd037113866fa7dfe300f1d0cfe
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-02 14:20:10 +0100

    m3 part1 finished

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,26 +1,27 @@
 // src/controllers/students.js
-
+import createHttpError from 'http-errors';
 import { getAllStudents, getStudentById } from '../services/students.js';
 
-export const getStudentsController = async (req, res) => {
-  const students = await getAllStudents();
+export const getStudentsController = async (req, res, next) => {
+  try {
+    const students = await getAllStudents();
 
-  res.json({
-    status: 200,
-    message: 'Successfully found students!',
-    data: students,
-  });
+    res.json({
+      status: 200,
+      message: 'Successfully found students!',
+      data: students,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const getStudentByIdController = async (req, res) => {
+export const getStudentByIdController = async (req, res, next) => {
   const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-    // Відповідь, якщо контакт не знайдено
+  const student = await getStudentById(studentId);
+  // Відповідь, якщо контакт не знайдено
   if (!student) {
-    res.status(404).json({
-      message: 'Student not found',
-    });
-    return;
+    throw createHttpError(404, 'Student not found');
   }
 
   // Відповідь, якщо контакт знайдено
--- src/routers/students.js
+++ src/routers/students.js
@@ -6,10 +6,11 @@ import {
   getStudentsController,
   getStudentByIdController,
 } from '../controllers/students.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper';
 
 const router = Router();
 
-router.get('/students', getStudentsController);
-router.get('/students/:studentId', getStudentByIdController);
+router.get('/students', ctrlWrapper(getStudentsController));
+router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 
 export default router;
