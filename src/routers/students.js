3621a923b6f50119445987c80e552cdd5d700882
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-08-02 11:43:55 +0100

    module 3 part 1

---
+++ src/routers/students.js
@@ -0,0 +1,15 @@
+// src/router/students.js
+
+import { Router } from 'express';
+
+import {
+  getStudentsController,
+  getStudentByIdController,
+} from '../controllers/students.js';
+
+const router = Router();
+
+router.get('/students', getStudentsController);
+router.get('/students/:studentId', getStudentByIdController);
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getAllStudents, getStudentById } from './services/students.js';
 
 import { env } from './utils/env.js';
+import studentsRouter from './routers/students.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -14,7 +14,6 @@ export const startServer = () => {
 
   app.use(express.json());
   app.use(cors());
-
   app.use(
     pino({
       transport: {
@@ -29,31 +28,7 @@ export const startServer = () => {
     });
   });
 
-    app.get('/students', async (req, res) => {
-      const students = await getAllStudents();
-
-      res.status(200).json({
-        data: students,
-      });
-    });
-
-    app.get('/students/:studentId', async (req, res, next) => {
-      const { studentId } = req.params;
-      const student = await getStudentById(studentId);
-
-      // Відповідь, якщо контакт не знайдено
-      if (!student) {
-        res.status(404).json({
-          message: 'Student not found',
-        });
-        return;
-      }
-
-      // Відповідь, якщо контакт знайдено
-      res.status(200).json({
-        data: student,
-      });
-    });
+  app.use(studentsRouter); // Додаємо роутер до app як middleware
 
   app.use('*', (req, res, next) => {
     res.status(404).json({
